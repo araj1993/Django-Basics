57cc175d8747793d27a613433f7e6a4cd64da0a7
araj1993 <araj20sep@gmail.com>
2025-11-09 12:12:32 +0530

    Django-Model-Forms: display inserted data in DB

--- django-normal-forms/LibraryManagement/library_admin/models.py
+++ django-normal-forms/LibraryManagement/library_admin/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+class AddBookInfo(models.Model):
+    title = models.CharField(max_length=100)
+    author = models.CharField(max_length=100)
+    genre = models.CharField(max_length=100)
+    published_date = models.DateField()
+    price = models.DecimalField(max_digits=6, decimal_places=3)
--- django-normal-forms/LibraryManagement/library_admin/views.py
+++ django-normal-forms/LibraryManagement/library_admin/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from .forms import BookInfoForm
+from .models import AddBookInfo
 
 
 def add_book_info(request):
@@ -12,7 +13,17 @@ def add_book_info(request):
             genre = forms.cleaned_data['genre']
             published_date = forms.cleaned_data['publication_date']
             price = forms.cleaned_data['price']
-        print(title, author, genre, published_date)
+        #normal forms
+        #print(title, author, genre, published_date)
+
+        #model forms: add data to table AddBookInfor
+        AddBookInfo.objects.create(
+            title = title, 
+            author = author, 
+            genre = genre, 
+            published_date = published_date, 
+            price = price)
+
     return render (request, 'add_book_info.html', {'form':forms})        
 
 
